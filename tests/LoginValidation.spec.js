e1dd6e8e9421e034a27238f4b1e17d1141e3fefe
Cristian Daddario <v-cdaddario@redrocktechsolutions.com>
2024-09-23 13:42:34 +0200

    missing spaces in README headers and added wait for selector in login validation

--- tests/LoginValidation.spec.js
+++ tests/LoginValidation.spec.js
@@ -22,7 +22,7 @@ test.describe('Login Feature Validation', () => {
 
         await test.step('Assert Successful Login', async () => {
             await page.waitForTimeout(5000);
-            await expect(loginPage.homeHeading).toHaveText("Home", { timeout: 10000 });
+            await expect(loginPage.homeHeading).toHaveText('Home', { timeout: 10000 });
             await expect(page).toHaveURL(testData.homePage);
         });
     });
